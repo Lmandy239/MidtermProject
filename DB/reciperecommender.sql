05ce4679ece9e0884c312374d76027fde33f591d
Daniel Riggi <daniel.l.riggi@gmail.com>
2024-02-02 14:01:35 -0500

    db update

--- DB/reciperecommender.sql
+++ DB/reciperecommender.sql
@@ -21,13 +21,13 @@ USE `reciperecommenderdb` ;
 DROP TABLE IF EXISTS `user` ;
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NULL DEFAULT NULL,
-  `enabled` TINYINT(4) NULL DEFAULT NULL,
+  `enabled` TINYINT NULL DEFAULT NULL,
   `role` VARCHAR(45) NULL DEFAULT NULL,
-  `createdat` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedat` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC))
 ENGINE = InnoDB
@@ -54,12 +54,20 @@ DROP TABLE IF EXISTS `recipe` ;
 
 CREATE TABLE IF NOT EXISTS `recipe` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `description` TEXT(255) NOT NULL,
-  `ingredient_description` TEXT(255) NOT NULL,
-  `createdat` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedat` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+  `name` VARCHAR(500) NOT NULL,
+  `description` TEXT NOT NULL,
+  `ingredient_description` TEXT NOT NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `recipe_image` VARCHAR(500) NULL,
+  `user_id` INT NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  INDEX `fk_recipe_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_recipe_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -69,11 +77,10 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `recipe_ingredient` ;
 
 CREATE TABLE IF NOT EXISTS `recipe_ingredient` (
-  `id` INT NOT NULL AUTO_INCREMENT,
   `ingredient_id` INT NOT NULL,
   `recipe_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
   INDEX `fk_recipe_ingredient_recipe1_idx` (`recipe_id` ASC),
+  PRIMARY KEY (`ingredient_id`, `recipe_id`),
   CONSTRAINT `fk_recipe_ingredient_ingredient`
     FOREIGN KEY (`ingredient_id`)
     REFERENCES `ingredient` (`id`)
@@ -93,11 +100,10 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `user_ingredient` ;
 
 CREATE TABLE IF NOT EXISTS `user_ingredient` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT(11) NOT NULL,
+  `user_id` INT NOT NULL,
   `ingredient_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
   INDEX `fk_user_ingredient_ingredient1_idx` (`ingredient_id` ASC),
+  PRIMARY KEY (`user_id`, `ingredient_id`),
   CONSTRAINT `fk_user_ingredient_user1`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
@@ -106,6 +112,55 @@ CREATE TABLE IF NOT EXISTS `user_ingredient` (
   CONSTRAINT `fk_user_ingredient_ingredient1`
     FOREIGN KEY (`ingredient_id`)
     REFERENCES `ingredient` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_comment` ;
+
+CREATE TABLE IF NOT EXISTS `user_comment` (
+  `recipe_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `comment` TEXT NULL,
+  PRIMARY KEY (`recipe_id`, `user_id`),
+  INDEX `fk_recipe_has_user_user1_idx` (`user_id` ASC),
+  INDEX `fk_recipe_has_user_recipe1_idx` (`recipe_id` ASC),
+  CONSTRAINT `fk_recipe_has_user_recipe1`
+    FOREIGN KEY (`recipe_id`)
+    REFERENCES `recipe` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_recipe_has_user_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `recipe_impression`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `recipe_impression` ;
+
+CREATE TABLE IF NOT EXISTS `recipe_impression` (
+  `recipe_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`recipe_id`, `user_id`),
+  INDEX `fk_recipe_has_user_user2_idx` (`user_id` ASC),
+  INDEX `fk_recipe_has_user_recipe2_idx` (`recipe_id` ASC),
+  CONSTRAINT `fk_recipe_has_user_recipe2`
+    FOREIGN KEY (`recipe_id`)
+    REFERENCES `recipe` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_recipe_has_user_user2`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -126,7 +181,75 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `reciperecommenderdb`;
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `createdat`, `updatedat`) VALUES (1, 'billybob', 'password', 1, 'user', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `created_at`, `updated_at`) VALUES (1, 'admin', 'admin', 1, 'admin', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `created_at`, `updated_at`) VALUES (2, 'armando', 'armando', 1, 'user', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `created_at`, `updated_at`) VALUES (3, 'daniel', 'daniel', 1, 'user', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `created_at`, `updated_at`) VALUES (4, 'jesse', 'jesse', 1, 'user', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `created_at`, `updated_at`) VALUES (5, 'blake', 'blake', 1, 'user', NULL, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `ingredient`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `reciperecommenderdb`;
+INSERT INTO `ingredient` (`id`, `name`) VALUES (1, 'test ingredient');
+INSERT INTO `ingredient` (`id`, `name`) VALUES (2, 'test ingredient 2');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `recipe`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `reciperecommenderdb`;
+INSERT INTO `recipe` (`id`, `name`, `description`, `ingredient_description`, `created_at`, `updated_at`, `recipe_image`, `user_id`) VALUES (1, 'test recipe name', 'description 1', 'ingredient description 1', NULL, NULL, NULL, DEFAULT);
+INSERT INTO `recipe` (`id`, `name`, `description`, `ingredient_description`, `created_at`, `updated_at`, `recipe_image`, `user_id`) VALUES (2, 'test recipe name 2', 'description 2', 'ingredient description 2', NULL, NULL, NULL, DEFAULT);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `recipe_ingredient`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `reciperecommenderdb`;
+INSERT INTO `recipe_ingredient` (`ingredient_id`, `recipe_id`) VALUES (1, 1);
+INSERT INTO `recipe_ingredient` (`ingredient_id`, `recipe_id`) VALUES (2, 2);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `user_ingredient`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `reciperecommenderdb`;
+INSERT INTO `user_ingredient` (`user_id`, `ingredient_id`) VALUES (2, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `user_comment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `reciperecommenderdb`;
+INSERT INTO `user_comment` (`recipe_id`, `user_id`, `comment`) VALUES (1, 1, 'This stuff sucks!');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `recipe_impression`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `reciperecommenderdb`;
+INSERT INTO `recipe_impression` (`recipe_id`, `user_id`) VALUES (1, 1);
+INSERT INTO `recipe_impression` (`recipe_id`, `user_id`) VALUES (1, 2);
 
 COMMIT;
 
