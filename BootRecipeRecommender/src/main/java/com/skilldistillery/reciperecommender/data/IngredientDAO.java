f7b0c6b800dc334a0e4514c9f964f99bf68b2c82
Daniel Riggi <daniel.l.riggi@gmail.com>
2024-02-06 20:00:10 -0500

    fixing mess up

---
+++ BootRecipeRecommender/src/main/java/com/skilldistillery/reciperecommender/data/IngredientDAO.java
@@ -0,0 +1,27 @@
+package com.skilldistillery.reciperecommender.data;
+
+import java.util.List;
+
+import com.skilldistillery.reciperecommender.entities.Ingredient;
+import com.skilldistillery.reciperecommender.entities.Recipe;
+import com.skilldistillery.reciperecommender.entities.User;
+
+public interface IngredientDAO {
+
+	List<Ingredient> findIngredientByName(User user, String namePattern);
+
+	void removeIngredient(User user, Ingredient ingredient);
+
+	List<Recipe> generateRecipes(User user);
+
+	void addToPantry(User user, Ingredient ingredient);
+
+	Ingredient findById(int id);
+	
+	List<Recipe> findAll();
+	
+	List<Recipe> findRecipesByIngredients(String name);
+	
+	public List<Recipe> allRecipe();
+	
+}
---
+++ BootRecipeRecommender/src/main/java/com/skilldistillery/reciperecommender/data/UserDAO.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.reciperecommender.data;
+
+import com.skilldistillery.reciperecommender.entities.User;
+
+public interface UserDAO {
+
+	User authenticateUser(String username, String password);
+
+	User registerUser(String username, String password);
+
+	void save(User user);
+
+	User findById(int id);
+
+}
